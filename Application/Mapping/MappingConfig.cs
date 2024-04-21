e474c0b0b7f55ce49b6af3a5d8578f8fca8302c2
SinaAbszde <sinaabszde83@gmail.com>
2024-04-21 16:24:55 +0330

    Added and configured Automapper

---
+++ Application/Mapping/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain.Models;
+
+namespace Application.Mapping
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Product, ProductCreateDTO>().ReverseMap();
+            CreateMap<Product, ProductUpdateDTO>().ReverseMap();
+        }
+    }
+}
