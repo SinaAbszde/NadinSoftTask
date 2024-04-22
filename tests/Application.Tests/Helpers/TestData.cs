85076c0fd39940bd90d4a44ed3bab712d79ef18f
SinaAbszde <sinaabszde83@gmail.com>
2024-04-22 15:58:31 +0330

    Added a helper class to generate Products and ProductDTOs

---
+++ tests/Application.Tests/Helpers/TestData.cs
@@ -0,0 +1,67 @@
+﻿using Application.DTOs;
+using Domain.Models;
+
+namespace Application.Tests.Helpers
+{
+    public class TestData
+    {
+        private static int _productCounter = 1;
+
+        public static Product GenerateProduct()
+        {
+            var product = new Product
+            {
+                Name = $"Test Product {_productCounter}",
+                ManufacturePhone = "1234567890",
+                ManufactureEmail = $"test{_productCounter}@example.com",
+                ProduceDate = DateOnly.FromDateTime(DateTime.Now.AddDays(_productCounter)),
+                IsAvailable = true
+            };
+
+            _productCounter++;
+            return product;
+        }
+
+        public static ProductDTO GenerateProductDTO()
+        {
+            var productDTO = new ProductDTO
+            {
+                ID = 1,
+                Name = "Test Product",
+                ManufacturePhone = "1234567890",
+                ManufactureEmail = "test@example.com",
+                ProduceDate = DateOnly.FromDateTime(DateTime.Now),
+                IsAvailable = true
+            };
+
+            return productDTO;
+        }
+
+        public static ProductCreateDTO GenerateProducCreatetDTO()
+        {
+            var productCreateDTO = new ProductCreateDTO
+            {
+                Name = "Test Product",
+                ManufacturePhone = "1234567890",
+                ManufactureEmail = "test@example.com",
+                IsAvailable = true
+            };
+
+            return productCreateDTO;
+        }
+
+        public static ProductUpdateDTO GenerateProductUpdateDTO()
+        {
+            var productUpdateDTO = new ProductUpdateDTO
+            {
+                ID = 1,
+                Name = "Test Product",
+                ManufacturePhone = "1234567890",
+                ManufactureEmail = "test@example.com",
+                IsAvailable = true
+            };
+
+            return productUpdateDTO;
+        }
+    }
+}
