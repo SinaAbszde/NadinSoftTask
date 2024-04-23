10aaef3edd73c6871f5ce7217e4d6bedd43d51c1
SinaAbszde <sinaabszde83@gmail.com>
2024-04-23 23:17:02 +0330

    Added DTOs for Registration and Login

---
+++ src/Application/DTOs/Auth/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Auth
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
