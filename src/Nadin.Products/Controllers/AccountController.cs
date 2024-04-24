a9e82a58cb0a2a53d5fb9418ea8d102ed8db9030
SinaAbszde <sinaabszde83@gmail.com>
2024-04-24 10:26:41 +0330

    Added the AccountController for auth actions

---
+++ src/Nadin.Products/Controllers/AccountController.cs
@@ -0,0 +1,129 @@
+﻿using Application.DTOs.Auth;
+using Application.Interfaces.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nadin.Products.Responses;
+using System.Net;
+
+namespace Nadin.Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IUserRegistrationService _registrationService;
+        private readonly IUserLoginService _loginService;
+        private readonly IUserLogoutService _logoutService;
+        protected APIResponse _response;
+
+        public AccountController(
+            IUserRegistrationService registrationService,
+            IUserLoginService loginService,
+            IUserLogoutService logoutService)
+        {
+            _registrationService = registrationService;
+            _loginService = loginService;
+            _logoutService = logoutService;
+            _response = new();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage)).ToList();
+                return BadRequest(_response);
+            }
+
+            try
+            {
+                var result = await _registrationService.RegisterUserAsync(model);
+
+                if (result.Succeeded)
+                {
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    _response.Result = "Registered successfully.";
+                    return Ok(_response);
+                }
+                else
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages = result.Errors.Select(e => e.Description).ToList();
+                    return BadRequest(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message };
+                return StatusCode(500, _response);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage)).ToList();
+                return BadRequest(_response);
+            }
+
+            try
+            {
+                var result = await _loginService.LoginAsync(model);
+
+                if (result.Succeeded)
+                {
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    _response.Result = "Logged in successfully.";
+                    return Ok(_response);
+                }
+                else
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages = ["There was an error while trying to Login."];
+                    return BadRequest(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message };
+                return StatusCode(500, _response);
+            }
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            try
+            {
+                await _logoutService.LogoutAsync();
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = "Logged out successfully.";
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message };
+                return StatusCode(500, _response);
+            }
+        }
+    }
+}
